c181a463ead270291c8ab5c77f58ab95b0fcac8c
Carlos Santana <csantana23@gmail.com>
2020-02-27 13:17:05 -0500

    made a change for the demo

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FastStart2020' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'FastStart2020 demo1' });
 });
 
 module.exports = router;
